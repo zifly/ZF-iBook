e1c3b74286bcd8decbf81e3d0e67d8df27218030
zifly <yuyi.cd@gmail.com>
2018-09-19 18:29:26 +0800

    update ZF-iBook

---
+++ bat/a.bat
@@ -0,0 +1,29 @@
+@echo off
+title 读秀书籍图片摘取器
+setlocal enabledelayedexpansion
+
+rem author:zifly77@163.com
+
+
+set nowpath=%~dp0
+
+cls
+echo/
+echo/
+echo                         读秀书籍图片摘取器
+echo/
+echo/
+echo       		============================================
+echo/
+
+echo                      1  清空IE浏览器缓存
+echo                      2  获取缓存中的图片
+echo                      3  交换图片序号
+echo                      4  图片序号递增递减
+echo                      5  标准化图片序号
+echo/
+echo                      0  退出
+echo/
+echo       		============================================
+echo/
+echo/
