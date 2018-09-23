92448eaa650c4d740ee6505c63e4e3a432f33aee
zifly <yuyi.cd@gmail.com>
2018-09-23 23:12:19 +0800

    modify the new menu

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ var path = require('path')
 
 program
      .version(require('./package').version)
-     .option('-i, --init', '初始化书籍文件夹')
-     .option('-d, --dev', '以开发模式启动coodev 即时编译 不压缩')
+     .option('-i, new', '初始化书籍文件夹')
+     .option('-l,--load', '加载书籍')
+     .option('-d, --dev', '即时编译 不压缩')
      .option('-b, --build', '以生产模式启动')
      .option('-u, --update', '更新书籍内容')
      .option('-p, --publish', '提交到发布仓库')
      .parse(process.argv);
  
  /** init */
- if (program.init) {
+ if (program.new) {
      require('./src/new-book.js');
  }
+ 
  
