194bb5cba3819e6015b1fc94e458af1760caf700
zifly <yuyi.cd@gmail.com>
2018-09-25 23:57:04 +0800

    input Chinese Ok.create book -the first step complete

--- src/new-book.js
+++ src/new-book.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
 
-//var download = require('download-git-repo')
-var program = require('commander')
-//var readline = require('readline');
-var readlineSync = require('readline-sync');
-var iconv = require("iconv-lite");
+var readline = require('readline');
+
 
 var fs=require('fs')
 var exists = require('fs').existsSync
@@ -14,175 +11,148 @@ var path = require('path')
 var arguments = process.argv;
 var bookname;
 var bookfolder;
+bookroot=process.cwd();
 
-for(i=0;i<arguments.length;i++){
-    //console.log(arguments[i]);
-}
-if(arguments.length<4){
-    
-    console.log('\x1B[31m',"\n ERROR:  请输入书籍存放的文件夹")
-    console.log("\x1B[37m","-----------------------------------")
-    console.log(" Grammar:  \x1B[32mibook \x1B[33mnew \x1B[36mfoldername\x1B[37m\n")
-    
-    
-}
-else{
-    donewbook();
-}
-
-
-
-function donewbook(){
-    //判断图书仓库是否存在，不存在就创建
-    //console.log("bf="+bf);
-    var libroot="_libarys";
-    if(exists(libroot)==false){
-        console.log("create libary storage:"+libroot);
-        fs.mkdirSync(libroot);
-        }
-    else console.log("have libary storage");
-    
-    //initbookconfig("");
-    
-    
-    /*判断书籍文件夹是否存在，不存在就创建
-    var bookroot=libroot+"/"+bf;
-    if(exists(bookroot)==false){
-        fs.mkdirSync(bookroot);
-        console.log("create book folder:"+bookroot)
-        //clonesource(bootroot);
-        
-    }
-    else console.log("have book folder->"+bookroot);
-    
-    
-   判断书籍config是否存在，不存在就创建*/
-        
-    
-    
-}
-  
-function initbookconfig(sfile){
     
     var tags=new Array(
         'book Name',
         'book Author',
-        'book Language',
-        'book Output-Encoding'
+        'book Language'
     )
     var values=new Array(
         '',
         '',
-        'zh',
-        'utf-8'
+        'zh'
     )
-    startnum=0;
-    
-    if(sfile!=""){
-        var bconfig=sfile+"/book.json";
-        if(!exists(bconfig)){
-            flag=0;
-            startnum=0;
-        }
-        else {
-            flag=1;
-            starnum=1
-        }
-        
-        if(flag==1){
-            var bookjson=fs.readFile(sfile,function(err,data){
-                if(err){
-                    console.log(err);
-                }
-                else{
-                    return JSON.parse(data.toString);
-                }
-            });
-            
-            var i=0;
-            for(var v in bookjson){
-                values[i]=bookjson[v];
-                i++;
-            }
-        }
-        
-    }
-    
     
 
-    for(i=startnum;i<tags.length;i++){
-        tag=tags[i];
-        ov=values[i];
-        if(ov!=0){
-            tag=tag+'('+ov+'): ';
-        }
-        else tag=tag+': ';
-        res=readlineSync.question(tag);
-        if(res!=""){
-            if(i==0) {
-                txt=iconv.encode(res,'utf8')
-                //fs.writeFileSync("a.txt",res)
-                //buf=new Buffer(res);
-                
-                }
-            
-            values[i]=res;
-        }
-    }
-    console.log("========================");
-    
-    for(i=0;i<tags.length;i++){
-        console.log(tags[i]+': '+values[i]);
-    }
-    console.log("========================");
-    buffer=fs.readFileSync('a.txt');
-    res=iconv.decode(buffer,'gbk');
-    console.log(res);
-    console.log(buffer.toString())
-}
-
-
-
-function clonesource(sfolder){
-    
-}
-
-function readFile(sfile){
-    console.log('--------开始读取文件--------');
-    fs.readFile(sfile, 'utf-8', function(err, data) {
-        if (err) {
-            console.log("读取失败");
-        } else {
-            console.log(data);
-            return data;
-        }
-    });
-    console.log('--------读取结束--------');
-}
-
-/*
-搞不定的异步，放一边吧
-readSync(labs).then((res) => {
-   bookname=res;
-   console.log(res);
+const rinput = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
 });
 
-function readSync(tips) {
-    tips = tips || '> ';
- 
-    return new Promise((resolve) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+var linenum=0;
+tag=tags[linenum];
+rinput.setPrompt('\x1B[32m'+tag+": \x1B[37m");
+rinput.prompt();
 
-        rl.question(tips, (answer) => {
-            rl.close();
-            resolve(answer.trim());
-        });
-    });
+
+rinput.on('line', (lineinput)=> {
+    linenum++;
+    //console.log(linenum);
+    tag=tags[linenum];
+    if(linenum==1){
+        if(lineinput==''){
+            rinput.setPrompt('');
+            console.log("\x1B[35mBook Name Can NOT be Null");
+            linenum--;
+            tag=tags[linenum];
+            
+        }
+        
+    }
+    
+        if(linenum>values.length-1) rinput.close();
+        if (linenum>0) values[linenum-1]=lineinput;
+        rinput.setPrompt('\x1B[32m'+tag+": \x1B[37m");
+        rinput.prompt();
+    
+    
+        
+});
+
+rinput.on('close',()=>{
+    
+    nowpath=process.cwd();
+    mybookpath=createfolder(nowpath,values[0]);
+    if(mybookpath!=""){
+        console.log("creating book init data... ...");
+        
+        jsonstr=getbookjson(values);
+        
+        fs.writeFileSync(mybookpath+'\\'+'book.json',jsonstr);
+        createfolder(mybookpath,'md');
+        
+        console.log('Create New book \x1B[33m'+values[0]+'\x1B[37m OK');
+    }
+    else{
+        console.log("Exists Book: \x1B[33m"+values[0]+"\x1B[37m");
+    }
+    process.exit(0);
+});
+    
+function createfolder(p,s){
+    //console.log("creating book folder... ...");
+    
+    s=foldername(s);
+    ff=p+'\\'+s;
+    if(exists(ff)==false){
+        fs.mkdirSync(ff);
+        //console.log("Create folder:  \x1B[33m"+ff+"\x1B[37m");
+        
+        return ff;
+    }
+    else{
+        //console.log("Exists Folder: \x1B[33m"+ff+"\x1B[37m");
+        return "";
+    }
 }
 
-*/
+function getbookjson(arr){
+    var labarr=new Array(
+        'title',
+        'creator',
+        'language',
+        'encoding',
+        'doctype',
+        'contributor',
+        'identifier'
+    );
+    var labvalues=new Array(
+        '',
+        '',
+        'zh',
+        'utf-8',
+        'EBOK',
+        'ZFiBook',
+        ''
+    );
+    
+    console.log("\r\nbook Info:")
+    console.log("=================================");
+    
+    str='   {\r\n';
+    for(ilab=0;ilab<labarr.length;ilab++){
+        if(ilab<arr.length){
+            labvalues[ilab]=arr[ilab]
+        }
+        strelement='        "'+labarr[ilab]+'":"'+labvalues[ilab]+'"';
+        if(ilab<labarr.length-1) strelement=strelement+',\r\n';
+        else strelement=strelement+'\r\n';
+        str=str+strelement;
+        console.log(labarr[ilab]+': '+labvalues[ilab]);
+    }
+    str=str+'   }\r\n';
+    
+    console.log("=================================");
+    
+    
+    
+    return str;
+}
 
+function foldername(s){
+    s=s.replace(/\\/ig,"_");
+    s=s.replace(/\*/ig,"_");
+    s=s.replace(/\?/ig,"_");
+    s=s.replace(/\"/ig,"");
+    s=s.replace(/\|/ig,"_");
+    s=s.replace(/\s/ig,"_");
+    s=s.replace(/\:/ig,"_");
+    s=s.replace(/\</ig,"_");
+    s=s.replace(/\>/ig,"_");
+    return s;
+    
+}
 
